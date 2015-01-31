2643c8e7a6f4d32a6d5e419b844dd49f2fe80b5e
toma678 <toma678@users.noreply.github.com>
2015-01-31 21:54:57 +0000

    Updated Compile.sh
    Re-added output filename, but automated from the input name.

--- Compile.sh
+++ Compile.sh
@@ -10,8 +10,8 @@ if [[ $1 != *.scss ]]
 then
 	echo "[*] Incorrect file extension on supplied input... Cancelling."
 else
-	CMD1="sass $1"
 	F2=${1%%.*}
+	CMD1="sass $1 $F2.css"
 	CMD2="sass $F2.css:$F2.min.css --style compressed --scss"
 	echo "[*] Compiling $1 into $F2.css."
 	$CMD1
