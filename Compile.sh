d115e063889835ba3df7145b4fdf861b26f9f0ff
toma678 <toma678@users.noreply.github.com>
2015-01-31 22:29:18 +0000

    Updated Compile.sh
    Cosmetic stuff.

--- Compile.sh
+++ Compile.sh
@@ -1,20 +1,23 @@
-# SASS compiler.
-# Written by Toma678.
+# Caramel Framework Compiler
+# Written by Toma678
+
 if [ -z "$1" ]
 then
-	echo "[*] Input file not supplied... Cancelling."
+	echo "[*] Syntax Error - Usage:"
+	echo "[*] Compiler.sh input.scss"
 	exit
 fi
 
 if [[ $1 != *.scss ]]
 then
-	echo "[*] Incorrect file extension on supplied input... Cancelling."
+	echo "[*] Incorrect File Type, Must be .SCSS"
 else
 	F2=${1%%.*}
 	CMD1="sass $1 $F2.css"
 	CMD2="sass $F2.css:$F2.min.css --style compressed --scss"
-	echo "[*] Compiling $1 into $F2.css."
+	echo "[*] Compiling..."
 	$CMD1
-	echo "[*] Creating compressed file."
+	echo "[*] Minifying..."
 	$CMD2
+	echo "[*] Done!"
 fi
