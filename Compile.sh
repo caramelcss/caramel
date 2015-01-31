f19777af1d7e659f803cd1f3f0dfb64e21ab56fc
toma678 <toma678@users.noreply.github.com>
2015-01-31 21:48:06 +0000

    Added compressed file creation
    Removed the 2nd file input (not needed), made script create compressed CSS as well as compiled.

--- Compile.sh
+++ Compile.sh
@@ -4,21 +4,17 @@ if [ -z "$1" ]
 then
 	echo "[*] Input file not supplied... Cancelling."
 	exit
-elif [ -z "$2" ]
-then
-	echo "[*] Output file not supplied... Cancelling."
-	exit
 fi
 
 if [[ $1 != *.scss ]]
 then
 	echo "[*] Incorrect file extension on supplied input... Cancelling."
-elif [[ $2 != *.css ]]
-then
-	echo "[*] Incorrect file extension on supplied output... Cancelling."
 else
-CMD="sass $1 $2"
-echo "[*] Compiling $1 into $2."
-$CMD
-exit
+	CMD1="sass $1"
+	F2=${1%%.*}
+	CMD2="sass $F2.css:$F2.min.css --style compressed --scss"
+	echo "[*] Compiling $1 into $F2.css."
+	$CMD1
+	echo "[*] Creating compressed file."
+	$CMD2
 fi
