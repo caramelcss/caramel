6a5049481978b2db74f2c4340986e76c6182c638
Penagwin <langpenguin@gmail.com>
2015-02-27 12:12:42 -0500

    Clostest not Parent

--- stylesheets/Caramel.js
+++ stylesheets/Caramel.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
     // Alert Dismissables
     $(".dismiss").click(function() {
-        $(this).parent("#note").fadeOut(500, function() {
+        $(this).closest("#note").fadeOut(500, function() {
             $(this).remove();
         });
     });
