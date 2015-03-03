e853e8d566f75ef0249aca84072e08730c492498
Henry <omgredstone@gmail.com>
2015-03-03 19:47:35 +0000

    Fixed the adding of jQuery
    Changed to .setAttribute() calls, which actually work.

--- stylesheets/Caramel.js
+++ stylesheets/Caramel.js
@@ -2,8 +2,8 @@
 // Javascript    - http://dev.penagw.in
 
 var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-2.1.1.min.js';
-script.type = 'text/javascript';
+script.setAttribute("src", 'http://code.jquery.com/jquery-2.1.1.min.js');
+script.setAttribute("type", 'text/javascript');
 document.getElementsByTagName('head')[0].appendChild(script);
 
 $(document).ready(function() {
