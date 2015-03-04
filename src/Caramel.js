17663efba36e19d671863bf202d6880b1803a0d8
KennethJackson <kenneth@whoiskenjackson.com>
2015-03-03 22:51:43 -0800

    Adding JS and tweaking some CSS to work in dropdown navigation

--- src/Caramel.js
+++ src/Caramel.js
@@ -3,43 +3,16 @@
 
 $(document).ready(function() {
 
-    /*$(".nav .bar").css({
-        "display": "block"
+    var $nav = $('.nav');
+
+    $nav.on('click', '.collapse', function(){
+        $(this).parents('ul').toggleClass('open');
     });
 
-    // Navbar Window Resize
-    $(window).resize(function() {
-        if ($(window).width() >= 800) {
-            $(".bar li").css({
-                "display": "inline-block"
-            });
-            $(".bar .collapse").css({
-                "display": "none"
-            });
-
-        } else {
-
-            $(".bar .collapse").css({
-                "display": "block"
-            });
-            $(".bar li").css({
-                "display": "none"
-            });
-            $(".bar").css({
-                "max-height": "inherit"
-            });
-        }
-    });*/
-
-    // On Expand Pressed
-    /*$(".bar .collapse").click(function() {
-        $(this).siblings('a').children('li').slideToggle('medium');
-
+    $nav.on('click', '.dropdown', function(e){
+        e.preventDefault();
+        $(this).parents('li').find('> ul').toggleClass('open');
     });
-    $(".dropdown").click(function() {
-        $(this).children('.hidden').slideToggle('medium');
-
-    });*/
 
     // Alert Dismissables
     $(".dismiss").click(function() {
