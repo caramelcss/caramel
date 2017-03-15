b19134d8f6aa3663960fdadf8d41f5dbcb8bd986
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-15 13:46:07 +1100

    Update Gulp Build Tool

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,28 @@
-const gulp = require("gulp")
-const sass = require("gulp-sass")
-const rename = require("gulp-rename")
-const uglify = require("gulp-uglify")
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const babel = require("gulp-babel");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
 
-gulp.task("default", ["javascript", "sass", "sass-minify"])
+gulp.task("default", ["js", "sass"]);
 
-gulp.task("javascript", () => {
-    gulp.src("./src/js/caramel.js")
+gulp.task("js", () =>
+    gulp.src("./js/caramel.js")
+        .pipe(babel())
+        .pipe(gulp.dest("./dist/js"))
         .pipe(uglify({ preserveComments: "license" }))
         .pipe(rename("caramel.min.js"))
-        .pipe(gulp.dest("./src/js"))
-})
+        .pipe(gulp.dest("./dist/js")));
 
-gulp.task("sass", () => {
-    gulp.src("./src/css/caramel.scss")
+gulp.task("sass", () =>
+    gulp.src("./scss/caramel.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(sass())
         .pipe(rename("caramel.css"))
-        .pipe(gulp.dest("./src/css"))
-})
-
-gulp.task("sass-minify", () => {
-    gulp.src("./src/css/caramel.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(sass({outputStyle: "compressed"}))
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(sass({ outputStyle: "compressed" }))
         .pipe(rename("caramel.min.css"))
-        .pipe(gulp.dest("./src/css"))
-})
+        .pipe(gulp.dest("./src/css")));
 
-gulp.task("sass:watch", () => {
-    gulp.watch("./src/css/**/*.scss", [ "sass", "sass-minify" ])
-})
+gulp.task("watch", () =>
+    gulp.watch("./scss/**/*.scss", ["sass"]));
