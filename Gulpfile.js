f2b3c6f9f9399ca85849a1576f4d8fc5a039ac42
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-16 15:27:29 +1100

    Add Babel ES2015 Preset, Watch for JS Files

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,11 +4,9 @@ const babel = require("gulp-babel");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
 
-gulp.task("default", ["js", "sass"]);
-
 gulp.task("js", () =>
     gulp.src("./js/caramel.js")
-        .pipe(babel())
+        .pipe(babel({ presets: ["es2015"] }))
         .pipe(gulp.dest("./dist/js"))
         .pipe(uglify({ preserveComments: "license" }))
         .pipe(rename("caramel.min.js"))
@@ -17,12 +15,12 @@ gulp.task("js", () =>
 gulp.task("sass", () =>
     gulp.src("./scss/caramel.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(sass())
+        .pipe(sass({ outputStyle: "expanded" }))
         .pipe(rename("caramel.css"))
         .pipe(gulp.dest("./dist/css"))
         .pipe(sass({ outputStyle: "compressed" }))
         .pipe(rename("caramel.min.css"))
-        .pipe(gulp.dest("./src/css")));
+        .pipe(gulp.dest("./dist/css")));
 
-gulp.task("watch", () =>
-    gulp.watch("./scss/**/*.scss", ["sass"]));
+gulp.task("watch", () => gulp.watch(["./scss/**/*.scss", "./js/**/*.js"], ["sass", "js"]));
+gulp.task("build", ["sass", "js"]);
